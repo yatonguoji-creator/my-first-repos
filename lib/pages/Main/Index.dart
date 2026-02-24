0c2b389df6f51ec8604d9684c776a9da035cbad8
yatonguoji <yatonguoji@gmail.com>
2026-02-24 10:49:49 +0800

    搭建底部导航栏代码

--- lib/pages/Main/Index.dart
+++ lib/pages/Main/Index.dart
@@ -8,11 +8,69 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  // 定义数据 根据数据进行渲染4个导航
+  // 一般应用程序的导航是固定的final
+  final List<Map<String, String>> _tabList = [
+    {
+      "icon": "lib/assets/ic_public_home_normal.png", // 正常显示的图标
+      "active_icon": "lib/assets/ic_public_home_active.png", // 激活显示的图标
+      "text": "首页",
+    },
+    {
+      "icon": "lib/assets/ic_public_pro_normal.png", // 正常显示的图标
+      "active_icon": "lib/assets/ic_public_pro_active.png", // 激活显示的图标
+      "text": "分类",
+    },
+    {
+      "icon": "lib/assets/ic_public_cart_normal.png", // 正常显示的图标
+      "active_icon": "lib/assets/ic_public_cart_active.png", // 激活显示的图标
+      "text": "购物车",
+    },
+    {
+      "icon": "lib/assets/ic_public_my_normal.png", // 正常显示的图标
+      "active_icon": "lib/assets/ic_public_my_active.png", // 激活显示的图标
+      "text": "我的",
+    },
+  ];
+  //声明底部导航栏变量
+  int _currentIndex = 0;
+
+  // 返回底部渲染的四个分类
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(_tabList.length, (int index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(
+          _tabList[index]["icon"]!,
+          width: 30,
+          height: 30,
+        ), // 正常显示图标
+        // 激活图标
+        activeIcon: Image.asset(
+          _tabList[index]["active_icon"]!,
+          width: 30,
+          height: 30,
+        ),
+        label: _tabList[index]["text"],
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("主页")),
       body: Center(child: Text("主页")),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red, //设置底部导航栏点击颜色
+        unselectedItemColor: Colors.black, //设置底部导航栏默认颜色
+        showUnselectedLabels: true, //设置底部导航栏默认颜色开关，必须为true
+        onTap: (int index) {
+          _currentIndex = index;
+          setState(() {});
+        },
+        currentIndex: _currentIndex,
+        items: _getTabBarWidget(),
+      ),
     );
   }
 }
